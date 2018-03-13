390bb91b6a77757000bb636172659ed61c096bd8
nearg1e <nearg1e.com@gmail.com>
2018-03-13 15:43:20 +0800

    fix: CreationDate may be nil in windows server 2008 or other

--- agent/collect/process_windows.go
+++ agent/collect/process_windows.go
@@ -4,65 +4,62 @@ package collect
 
 import (
 	"fmt"
-	"os"
+	"strconv"
 	"time"
 	"log"
 	"github.com/StackExchange/wmi"
 )
 
+// 在 Windows 2003 里有可能无法获得创建时间：CreationDate
+// 实际测试过程中, Windows 2008 R2 也有可能无法获取到时间导致错误
 type process struct {
 	Name            string
 	CommandLine     *string
 	ProcessId       uint32
 	ParentProcessId uint32
-	CreationDate    time.Time
+	CreationDate    *string
 }
-type process2003 struct {
-	Name            string
-	CommandLine     *string
-	ProcessId       uint32
-	ParentProcessId uint32
-}
+
 
 // GetProcessList 获取当前进程列表，与monitor process同类，故只用于保存显示
 func GetProcessList() (resultData []map[string]string) {
-	var m map[string]string
-	if _, err := os.Stat(os.Getenv("SystemDrive") + `/Users`); err != nil {
-		var dst []process2003
-		err := wmi.Query("SELECT * FROM Win32_Process", &dst)
-		if err != nil {
-			log.Println("Windows2003 get process info wmi Query error:", err)
-			return
-		}
-		if len(dst) != 0 {
-			for _, v := range dst {
-				m = make(map[string]string)
-				m["name"] = v.Name
-				m["pid"] = fmt.Sprintf("%d", v.ProcessId)
-				m["ppid"] = fmt.Sprintf("%d", v.ParentProcessId)
-				m["command"] = *v.CommandLine
-				m["starttime"] = ""
-				resultData = append(resultData, m)
-			}
-		}
-	} else {
-		var dst []process
-		err := wmi.Query("SELECT * FROM Win32_Process", &dst)
-		if err != nil {
-			log.Println("Windows get process info wmi Query error:",err)
-			return
-		}
-		if len(dst) != 0 {
-			for _, v := range dst {
-				m = make(map[string]string)
-				m["name"] = v.Name
-				m["pid"] = fmt.Sprintf("%d", v.ProcessId)
-				m["ppid"] = fmt.Sprintf("%d", v.ParentProcessId)
-				m["command"] = *v.CommandLine
-				m["starttime"] = v.CreationDate.String()
-				resultData = append(resultData, m)
-			}
+	var dst []process
+	err := wmi.Query("SELECT * FROM Win32_Process", &dst)
+	if err != nil {
+		log.Println("Windows get process info wmi Query error:",err)
+		return
+	}
+	if len(dst) != 0 {
+		for _, v := range dst {
+			m := make(map[string]string)
+			m["name"] = v.Name
+			m["pid"] = fmt.Sprintf("%d", v.ProcessId)
+			m["ppid"] = fmt.Sprintf("%d", v.ParentProcessId)
+			m["command"] = *v.CommandLine
+			m["starttime"] = parseTime(*v.CreationDate)
+			log.Println("Process data:", m)
+			resultData = append(resultData, m)
 		}
 	}
 	return resultData
+}
+
+// parseTime 格式化时间字符串
+func parseTime(val string) string {
+	if val == "" {
+		return ""
+	}
+	if len(val) == 25 {
+		// 数据格式例如 20180312092136.238259+480
+		mins, err := strconv.Atoi(val[22:])
+		if err != nil {
+			return ""
+		}
+		val = val[:22] + fmt.Sprintf("%02d%02d", mins/60, mins%60)
+	}
+	t, err := time.Parse("20060102150405.000000-0700", val)
+	if err != nil {
+		return ""
+	}
+	return t.String()
 }
