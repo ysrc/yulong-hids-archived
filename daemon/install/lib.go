91c1c5890ff5fafaaa922322d3056d674d5c788c
nearg1e <nearg1e.com@gmail.com>
2018-03-16 12:00:25 +0800

    add feature: agent文件完整性检查和下载重试

--- daemon/install/lib.go
+++ daemon/install/lib.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"crypto/md5"
 	"log"
 	"net/http"
 	"os"
@@ -78,7 +80,58 @@ func copyMe(installPath string) (err error) {
 
 // DownAgent 下载agent到指定安装目录
 func DownAgent(ip string, agentPath string, arch string) error {
+	var err error
 	url := fmt.Sprintf("%s://%s/json/download?system=%s&platform=%s&type=agent&action=download", common.Proto, ip, runtime.GOOS, arch)
-	err := downFile(url, agentPath)
-	return err
+
+	// Agent 下载检查和重试, 重试三次，功能性考虑
+	times := 3
+	for {
+		err = downFile(url, agentPath)
+		// 检查文件hash是否匹配
+		if err == nil {
+			mstr, _ := FileMD5String(agentPath)
+			log.Println("Agent file MD5:", mstr)
+			if CheckAgentHash(mstr, ip, arch) {
+				log.Println("Agent download finished, hash check passed")
+				return nil
+			} else {
+				log.Println("Agent is broken, retry the downloader again")
+			}
+		}
+		if times--; times == 0 {
+			break
+		}
+	}
+
+	return errors.New("Agent Download Error")
+}
+
+// FileMD5String 获取文件MD5
+func FileMD5String(filePath string) (MD5String string, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	md5h := md5.New()
+	io.Copy(md5h, file)
+	return fmt.Sprintf("%x", md5h.Sum([]byte(""))), nil
+}
+
+
+// CheckAgentHash 检查Agent的哈希值是否匹配
+func CheckAgentHash(fileHash string, ip string, arch string) (is bool) {
+	checkURL := fmt.Sprintf(
+		"%s://%s/json/download?hash=%x&system=%s&platform=%s&action=check&type=agent",
+		common.Proto, ip, fileHash, runtime.GOOS, arch,
+	)
+	res, err := common.HTTPClient.Get(checkURL)
+	if err != nil {
+		return false
+	}
+	result, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return false
+	}
+	return "1" == string(result)
 }
